5fd6822b6299c5d48207fec2cc1cb36d40130f55
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-09 18:02:37 +0800

    Add robots config

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://jonathan-broqueza.vercel.app/sitemap.xml",
+  };
+}
