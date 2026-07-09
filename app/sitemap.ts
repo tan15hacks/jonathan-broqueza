ea91214500e77fc2d27d55a724f247ea80295a2c
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-09 19:07:30 +0800

    Remove admin page from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,11 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: "https://jonathan-broqueza.vercel.app/admin",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.2,
-    },
   ];
 }
